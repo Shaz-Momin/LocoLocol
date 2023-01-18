2f10d4dab861e60e1081af5aac198e67692b6f10
SkyRocket456 <skyrocket456@gmail.com>
2023-01-18 14:46:49 -0600

    - Homepage is now scrollable

---
+++ frontend/locol-app/src/App.js
@@ -0,0 +1,7 @@
+import { Homepage } from "./homepage/homepage";
+
+export default function App() {
+    document.body.style.backgroundColor = "#000000";
+    document.body.style.margin = "0";
+    return <Homepage />;
+}
--- frontend/locol-app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { Homepage_Root } from "./homepage";
-
-export default function App() {
-  document.body.style.backgroundColor = "#1e1e1e";
-  document.body.style.margin = "0";
-  return <Homepage_Root />;
-}
